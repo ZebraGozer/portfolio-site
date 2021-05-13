ef56eb4b29bb87a95cce510c3cede63de5a6ab30
LeviFX <82326386+LeviFX@users.noreply.github.com>
2021-05-13 18:41:41 +0200

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,25 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+if (empty($name || $email || $subject || $message)) {
+
+    die();
+
+} else {
+
+    $mailTo = "";
+    $headers = "From: " . $email;
+    $onderwerp = "Portfolio Contact: " . $subject;
+
+    mail($mailTo, $onderwerp, $message, $headers);
+
+}
+
+
+
+?>
+
